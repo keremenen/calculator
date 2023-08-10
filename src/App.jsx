0ed91a9f95ce95b630e275b5bdf3f0a154d20530
Przemyslaw <kitowski.przemek@gmail.com>
2023-08-10 14:23:46 +0200

    Base html structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,33 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      hello app
-    </>
+    <div className='App'>
+      <div className='wrapper'>
+        <div className='display'>
+          <span>(0)</span>0
+        </div>
+        <div className='controls'>
+          <button>/</button>
+          <button>*</button>
+          <button>-</button>
+          <button>+</button>
+          <button>DEL</button>
+        </div>
+        <div className='digits'>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
