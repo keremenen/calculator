57834f917602f10b7cffd22b0c9d16fb2960e399
Kemer <kitowski.przemek@gmail.com>
2023-08-10 16:43:16 +0200

    Added controled inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,49 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [calc, setCalc] = useState('')
+  const [restult, setResult] = useState('')
+
+  const controls = ['/', '*', '-', '+']
+
+  const handleClick = value => {
+    if (
+      (controls.includes(value) && calc === '') ||
+      (controls.includes(value) && controls.includes(calc.slice(-1)))
+    ) return
+    setCalc(prevState => {
+      return prevState + value
+    })
+  }
+
 
   return (
     <div className='App'>
       <div className='wrapper'>
         <div className='display'>
-          <span>(0)</span>0
+          <span>(0)</span>{calc}
         </div>
         <div className='controls'>
-          <button>/</button>
-          <button>*</button>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={() => handleClick('/')}>/</button>
+          <button onClick={() => handleClick('*')}>*</button>
+          <button onClick={() => handleClick('-')}>-</button>
+          <button onClick={() => handleClick('+')}>+</button>
           <button>DEL</button>
         </div>
         <div className='digits'>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>.</button>
+          <button onClick={() => handleClick(1)}>1</button>
+          <button onClick={() => handleClick(2)}>2</button>
+          <button onClick={() => handleClick(3)}>3</button>
+          <button onClick={() => handleClick(4)}>4</button>
+          <button onClick={() => handleClick(5)}>5</button>
+          <button onClick={() => handleClick(6)}>6</button>
+          <button onClick={() => handleClick(7)}>7</button>
+          <button onClick={() => handleClick(8)}>8</button>
+          <button onClick={() => handleClick(9)}>9</button>
+          <button onClick={() => handleClick(0)}>0</button>
+          <button onClick={() => handleClick('.')}>.</button>
           <button>=</button>
         </div>
       </div>
